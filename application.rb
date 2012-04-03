29417328018820d82806da3c174122359dc95b15
Ojima Hikaru <ojiam.h@gmail.com>
2012-04-04 00:27:13 +0900

    mv 404.html to public

--- application.rb
+++ application.rb
@@ -22,7 +22,7 @@ class Application
     if File::exists? path
       [200, {"Content-Type" => "text/html"}, [convert(path)]]
     else
-      [404, {"Content-Type" => "text/html"}, [File::read("404.html")]]
+      [404, {"Content-Type" => "text/html"}, [File::read("public/404.html")]]
     end
   end
 end
